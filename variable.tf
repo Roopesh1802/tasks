7513c23bcd3c5ff39df482199250669b7fc4fd92
Roopesh1802 <85818834+Roopesh1802@users.noreply.github.com>
2023-04-24 10:50:43 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,55 @@
-variable "project_id" {
-  description = "The ID of the project to host the resouces"
-  type        = string
+variable "project-name" {
+    type = string
+    default = "vpc-network-jenkins"
 }
 
-variable "region" {
-  description = "The region in which to create the vpc network"
-  type        = string
+variable "firewall-name" {
+    type = string
+    default = "test-firewall"
+}
+
+variable "subnet1-name" {
+    type = string
+    default = "subnet-3"
+}
+
+variable "subnet1-cidr" {
+    type = string
+    default = "10.20.0.0/24"
+}
+
+variable "subnet1-region" {
+    type = string
+    default = "us-central1"
+}
+
+variable "subnet2-name" {
+    type = string
+    default = "subnet-4"
+}
+
+variable "subnet2-cidr" {
+    type = string
+    default = "10.32.0.0/24"
+}
+
+variable "subnet2-region" {
+    type = string
+    default = "us-west1"
+}
+
+
+variable "instance-name" {
+    type = string
+    default = "test-sample"
+}
+
+variable "zone" {
+    type = string
+    default = "us-central1-f"
+}
+
+variable "health-check" {
+    type = string
+    default = "healthcheck-http"
 }
